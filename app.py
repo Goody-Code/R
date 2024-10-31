aace22eb24a815dcdde8bc0f5e002cff9f17021a
Goody-Code <85070172+Goody-Code@users.noreply.github.com>
2024-10-31 05:53:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     syslog_thread.start()
 
     # تشغيل تطبيق Flask
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=9090, debug=True)
